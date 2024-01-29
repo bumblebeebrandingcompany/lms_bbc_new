ab073ac2e8d03b948970585867dcfebfc0cbad4a
bumblebeebrandingcompany <ahead@thebumblebee.in>
2024-01-29 19:13:36 +0530

    29.01.2024 n1

--- resources/views/admin/leads/partials/followup.blade.php
+++ resources/views/admin/leads/partials/followup.blade.php
@@ -1,34 +1,72 @@
-<h3 class="card-title"> Lead ID: {{ $lead->id }}</h3>
-<br>
-<div class="table-responsive">
+@if (auth()->user()->is_superadmin || auth()->user()->is_presales)
+    <h3 class="card-title"> Lead ID: {{ $lead->id }}</h3>
+    <br>
+    <div class="table-responsive">
 
-<table class="table">
-    <thead>
-        <tr>
-            <th>Campaign Name</th>
-            <th>Follow-Up Time</th>
-            <th>Follow-Up Date</th>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Campaign Name</th>
+                    <th>Follow-Up Time</th>
+                    <th>Follow-Up Date</th>
 
-<th>Notes</th>
-<th>Created at</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach ($followUps as $followUp)
-            <tr>
-                <td>{{ $lead->campaign->campaign_name }}</td>
-                <td>{{ $followUp->follow_up_time }}</td>
-                <td>{{ $followUp->follow_up_date }}</td>
+                    <th>Notes</th>
+                    <th>Created at</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($followUps->where('parent_stage_id', 9) as $followUp)
+                    <tr>
+                        <td>{{ $lead->campaign->campaign_name }}</td>
+                        <td>{{ $followUp->follow_up_time }}</td>
+                        <td>{{ $followUp->follow_up_date }}</td>
 
 
-                <td>{{ $followUp->notes }}</td>
-                <td>{{ $followUp->created_at }}</td>
+                        <td>{{ $followUp->notes }}</td>
+                        <td>{{ $followUp->created_at }}</td>
 
-            </tr>
-        @endforeach
-    </tbody>
-</table>
-</div>
-<div class="d-flex justify-content-end">
-    {{ $followUps->links('pagination::bootstrap-4') }}
-</div>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+    <div class="d-flex justify-content-end">
+        {{ $followUps->links('pagination::bootstrap-4') }}
+    </div>
+@endif
+@if (auth()->user()->is_admissionteam)
+    <h3 class="card-title"> Lead ID: {{ $lead->id }}</h3>
+    <br>
+    <div class="table-responsive">
+
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Campaign Name</th>
+                    <th>Follow-Up Time</th>
+                    <th>Follow-Up Date</th>
+
+                    <th>Notes</th>
+                    <th>Created at</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($followUps->where('parent_stage_id', 28) as $followUp)
+                    <tr>
+                        <td>{{ $lead->campaign->campaign_name }}</td>
+                        <td>{{ $followUp->follow_up_time }}</td>
+                        <td>{{ $followUp->follow_up_date }}</td>
+
+
+                        <td>{{ $followUp->notes }}</td>
+                        <td>{{ $followUp->created_at }}</td>
+
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+    <div class="d-flex justify-content-end">
+        {{ $followUps->links('pagination::bootstrap-4') }}
+    </div>
+@endif
